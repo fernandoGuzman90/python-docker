0a12184db5d90bb8212d07c9c73472309d85869a
fernandoGuzman90 <fernando.guzman@mh.gob.sv>
2024-05-27 15:10:02 -0600

    metodos suma

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hola, Atunes soft!'
+def index():
+    return render_template('index.html')
+
+@app.route('/sum', methods=['POST'])
+def sum_numbers():
+    num1 = request.form['num1']
+    num2 = request.form['num2']
+    try:
+        sum_result = float(num1) + float(num2)
+        return render_template('index.html', result=sum_result, num1=num1, num2=num2)
+    except ValueError:
+        return render_template('index.html', error="Invalid input. Please enter numbers.", num1=num1, num2=num2)
+
+if __name__ == '__main__':
+    app.run(debug=True)
